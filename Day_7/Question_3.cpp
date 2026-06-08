0de70cadcf3f8f22c7e22c31c0e55e6e1d99133c
Shree  Om Dubey <shreeomdubeydubey@gmail.com>
2026-06-08 15:52:53 +0530

    Add files via upload

---
+++ Day_7/Question_3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int sum_of_digits(int n)
+{
+  if(n==0)
+  {
+    return 0;
+  }
+    int digit=n%10;
+    return (digit+sum_of_digits(n/10));
+}
+int main()
+{
+ int n;
+ cout<<"Enter the value of n: ";
+ cin>>n;
+ cout<<"sum of digits is: "<<sum_of_digits(n);
+ return 0;
+
+}
